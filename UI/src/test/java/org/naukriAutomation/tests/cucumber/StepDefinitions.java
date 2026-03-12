a1be28c03940a2a5e63136f7ed33d909ed5bb2d1
sdet-unmesh-temkar <unmeshtemkar@zohomail.in>
2026-03-12 18:56:36 +0530

    step def update

--- UI/src/test/java/org/naukriAutomation/tests/cucumber/StepDefinitions.java
+++ UI/src/test/java/org/naukriAutomation/tests/cucumber/StepDefinitions.java
@@ -5,21 +5,18 @@ import io.cucumber.java.en.*;
 public class StepDefinitions {
     @Given("We launch the naukri application")
     public void we_launch_the_naukri_application() {
-        System.out.println("step 1 running");
+        System.out.println("step running");
     }
     @Given("We enter login credentials")
     public void we_enter_login_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("step running");
     }
     @When("We update the profile details")
     public void we_update_the_profile_details() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("step running");
     }
     @Then("We verify the profile is updated successfully")
     public void we_verify_the_profile_is_updated_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("step running");
     }
 }
